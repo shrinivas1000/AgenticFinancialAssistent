ee2156ea597dc6e8351403e788124684573cc3ab
shrinivas baskar <84432793+shrinivas1000@users.noreply.github.com>
2025-06-01 21:36:49 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import pyttsx3
-
-engine = pyttsx3.init()
-engine.setProperty('rate', 150)
-engine.save_to_file('Testing voice output', 'test_output.wav')
-engine.runAndWait()
-print("Audio file generated")
